76cd659206707a55cad30f35d7382d7a6e1e38ee
Raphael Da Silva <rapha.santi@gmail.com>
2024-11-11 07:51:21 -0300

    POST

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 import express from "express"
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id:"01",
+        name:"O senhor dos aneis"
+    },
+    {
+        id:"02",
+        name:"O Hobbit"
+    }
+]
+
 
 app.get("/", (req,res) =>{
     res.status(200).send("Curso De Node");
 })
 
+app.get("/livros", (req,res) =>{
+    res.status(200).json(livros)
+})
+app.post("/livros", (req,res) =>{
+    livros.push(req.body)
+    res.status(201).send("livros cadastrados com sucesso")
+});
 
 export default app;
